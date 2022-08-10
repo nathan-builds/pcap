6ae97b43fe63071ca668195e0132604b0408be17
nathan-builds <61122637+nathan-builds@users.noreply.github.com>
2022-08-10 15:29:24 -0400

    Update Main.java

--- PcapPlayer/src/main/java/Main.java
+++ PcapPlayer/src/main/java/Main.java
@@ -14,41 +14,44 @@ public class Main {
         InetAddress group = InetAddress.getByName("239.5.192.4");
         MulticastSocket socket = new MulticastSocket(1581);
         socket.joinGroup(group);
+        int numOfTimesToReadFile = 5;
 
+        for (int i = 0; i < numOfTimesToReadFile; i++) {
+          
+            pcap.loop(new PacketHandler() {
 
-        pcap.loop(new PacketHandler() {
+                @Override
+                public boolean nextPacket(Packet packet) throws IOException {
 
-            @Override
-            public boolean nextPacket(Packet packet) throws IOException {
+                    if (packet.hasProtocol(Protocol.TCP)) {
 
-                if (packet.hasProtocol(Protocol.TCP)) {
-
-                    TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
-                    Buffer buffer = tcpPacket.getPayload();
-                    if (buffer != null) {
-                        System.out.println("TCP: " + buffer);
-                    }
-                } else if (packet.hasProtocol(Protocol.UDP)) {
-
-                    UDPPacket udpPacket = (UDPPacket) packet.getPacket(Protocol.UDP);
-                    if (udpPacket.getDestinationPort() == 1581) {
-                        Buffer buffer = udpPacket.getPayload();
-
+                        TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
+                        Buffer buffer = tcpPacket.getPayload();
                         if (buffer != null) {
-                            byte[] payload = buffer.getArray();
-                            DatagramPacket msg = new DatagramPacket(payload, payload.length,
-                                    group, 1581);
-                            socket.send(msg);
-
-
+                            System.out.println("TCP: " + buffer);
                         }
+                    } else if (packet.hasProtocol(Protocol.UDP)) {
+
+                        UDPPacket udpPacket = (UDPPacket) packet.getPacket(Protocol.UDP);
+                        if (udpPacket.getDestinationPort() == 1581) {
+                            Buffer buffer = udpPacket.getPayload();
+
+                            if (buffer != null) {
+                                byte[] payload = buffer.getArray();
+                                DatagramPacket msg = new DatagramPacket(payload, payload.length,
+                                        group, 1581);
+                                socket.send(msg);
+
+
+                            }
+                        }
+
+
                     }
-
-
+                    return true;
                 }
-                return true;
-            }
-        });
+            });
+        }
         socket.close();
     }
 }
